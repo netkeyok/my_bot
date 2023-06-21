0ea122f5267708b514a880f9f3a15f64c0751d9e
Rinat <netkeyok@gmail.com>
2023-06-21 14:37:26 +0500

    Исправления

--- bot_config.py
+++ bot_config.py
@@ -3,6 +3,9 @@ import os
 
 bot_token = os.getenv('BOT_TOKEN')
 gpt_key = os.getenv('GPT_KEY')
+admin_id = '460379779'
+max_tokens = '4096'
+usage_check_frequency = '100'
 
 # class Settings(BaseSettings):
 #     # Желательно вместо str использовать SecretStr
